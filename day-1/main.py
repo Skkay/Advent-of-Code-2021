1dff71e4a64a74386e172e7e934d1cbe786405f8
Skkay <skkay.thomas@gmail.com>
2021-12-01 18:39:22 +0100

    Day 1 - Part 2

--- day-1/main.py
+++ day-1/main.py
@@ -1,14 +1,18 @@
 with open('day-1/inputs.txt', 'r') as f:
     inputs = f.readlines()
 
-    last = int(inputs[0])
-    inputs.pop(0)
+    last = int(inputs[0]) + int(inputs[1]) + int(inputs[2])
 
     count = 0
-    for line in inputs:
-        if int(line) > last:
-            count += 1
+    for index, line in enumerate(inputs):
+        try:
+            current = int(inputs[index]) + int(inputs[index + 1]) + int(inputs[index + 2])
 
-        last = int(line)
+            if current > last:
+                count += 1
+            
+            last = current
+        except IndexError:
+            print("Not enough data")
 
     print(count)
