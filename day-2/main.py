f9d3fe0b9ae31cb755b94af4552d95459feb1df0
Skkay <skkay.thomas@gmail.com>
2021-12-02 19:00:50 +0100

    Day 2 - Part 2

--- day-2/main.py
+++ day-2/main.py
@@ -1,15 +1,16 @@
 with open('day-2/inputs.txt', 'r') as f:
     inputs = f.readlines()
     
-    total = { 'h_position': 0, 'depth': 0 }
+    total = { 'h_position': 0, 'depth': 0, 'aim': 0 }
     for line in inputs:
         dir, value = line.split(' ')
 
         if dir == 'forward':
             total['h_position'] += int(value)
+            total['depth'] += total['aim'] * int(value)
         elif dir == 'down':
-            total['depth'] += int(value)
+            total['aim'] += int(value)
         elif dir == 'up':
-            total['depth'] -= int(value)
+            total['aim'] -= int(value)
     
     print(total['h_position'] * total['depth'])
