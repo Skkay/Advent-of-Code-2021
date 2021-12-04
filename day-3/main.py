b4c4abf5b73e26386f6c1fb5011aa797cf33d92e
Skkay <skkay.thomas@gmail.com>
2021-12-04 10:50:24 +0100

    Day 3 - Part 2

--- day-3/main.py
+++ day-3/main.py
@@ -2,19 +2,44 @@ with open('day-3/inputs.txt', 'r') as f:
     inputs = f.readlines()
     cols = len(inputs[0]) - 1 # skip \n
 
-    gamma = ""
-    epsilon = ""
+    oxygen_generator = inputs.copy()
+    co2_scrubber = inputs.copy()
 
     for i in range(0, cols):
         zero_occ =  0
         one_occ = 0
-        for line in inputs:
+
+        for line in oxygen_generator:
             if line[i] == '0':
                 zero_occ += 1
             else:
                 one_occ += 1
         
-        gamma += '0' if zero_occ > one_occ else '1'
-        epsilon += '1' if zero_occ > one_occ else '0'
-    
-    print(int(gamma, 2) * int(epsilon, 2))
+        if zero_occ > one_occ:
+            for line in oxygen_generator.copy():
+                if len(oxygen_generator) > 1 and line[i] == '1':
+                    oxygen_generator.remove(line)
+        else:
+            for line in oxygen_generator.copy():
+                if len(oxygen_generator) > 1 and line[i] == '0':
+                    oxygen_generator.remove(line)
+
+        zero_occ =  0
+        one_occ = 0
+        for line in co2_scrubber:
+            if line[i] == '0':
+                zero_occ += 1
+            else:
+                one_occ += 1
+        
+        if zero_occ > one_occ:
+            for line in co2_scrubber.copy():
+                if len(co2_scrubber) > 1 and line[i] == '0':
+                    co2_scrubber.remove(line)
+        else:
+            for line in co2_scrubber.copy():
+                if len(co2_scrubber) > 1 and line[i] == '1':
+                    co2_scrubber.remove(line)
+        
+
+    print(int(oxygen_generator[0], 2) * int(co2_scrubber[0], 2))
